e047d88e48f7654c274d8bb0bb28bf30f1d9936f
m-hatch <hatch@hq.acm.org>
2018-06-08 10:37:50 -0400

    dispatch actions

--- index.js
+++ index.js
@@ -12,5 +12,17 @@ const printState = () => {
 // print state on state change
 zoo.subscribe(printState)
 
+// dispatch actions to zoo
 console.log('print initial state')
 printState()
+
+console.log('add animals')
+zoo.dispatch(actions.addAnimal('Lion'))
+zoo.dispatch(actions.addAnimal('Tiger'))
+zoo.dispatch(actions.addAnimal('Bear'))
+
+console.log('set isOpenToPublic to true')
+zoo.dispatch(actions.setPublicStatus('Bear', true))
+
+console.log('remove animals')
+zoo.dispatch(actions.removeAnimal('Lion'))
