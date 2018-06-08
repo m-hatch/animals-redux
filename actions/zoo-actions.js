743b998df0e2fae306084df03ad31f18b2f57d53
m-hatch <hatch@hq.acm.org>
2018-06-08 10:28:54 -0400

    add actions

--- actions/zoo-actions.js
+++ actions/zoo-actions.js
@@ -1,11 +1,26 @@
 module.exports.addAnimal = (name) => {
-  
+  return { 
+    type: 'ADD_ANIMAL', 
+    animal: { 
+      name: name, 
+      isOpenToPublic: false 
+    }
+  }
 }
 
 module.exports.setPublicStatus = (name, status) => {
-  
+  return { 
+    type: 'SET_PUBLIC_STATUS', 
+    animal: { 
+      name: name, 
+      isOpenToPublic: status 
+    }
+  }
 }
 
 module.exports.removeAnimal = (name) => {
-  
+  return {
+    type: 'REMOVE_ANIMAL',
+    name
+  }
 }
